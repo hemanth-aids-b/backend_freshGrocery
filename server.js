cc047b28aaed8a1f52db7ee1e94580e3ad2f584e
hemanth-aids-b <hemanth.ka2024aids@sece.ac.in>
2025-12-17 15:59:06 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://hemanth-ecommerce-shopping.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
